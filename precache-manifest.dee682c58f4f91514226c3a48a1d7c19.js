654e65217592cc071ed07ee1e508936aaf120af5
Yogesh Raj Kabilan <rajyogesh46@gmail.com>
2020-05-24 01:17:08 +0530

    Updates

---
+++ precache-manifest.dee682c58f4f91514226c3a48a1d7c19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc4b16d7dda532116512c8b0b2ece52",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "18f3ef1499e32745b857",
+    "url": "/covid-19/static/css/main.ae0b7b08.chunk.css"
+  },
+  {
+    "revision": "0fd2ea69a9614d27c0f2",
+    "url": "/covid-19/static/js/2.f1c2c370.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.f1c2c370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f3ef1499e32745b857",
+    "url": "/covid-19/static/js/main.ddebf3e7.chunk.js"
+  },
+  {
+    "revision": "8c82da2f57d7c5632079",
+    "url": "/covid-19/static/js/runtime-main.876eddea.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
